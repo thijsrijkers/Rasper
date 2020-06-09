b7a32e7268af7b8338dedde8d269d6fa037c8b2d
thijsrijkers <gamearea20@gmail.com>
2020-06-09 16:37:41 +0200

    Begin build of bot

---
+++ Rasper/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Lavalink;
+using DSharpPlus.Lavalink.EventArgs;
+
+namespace Rasper
+{
+    public class Command : BaseCommandModule
+    {
+        private LavalinkExtension LavalinkService { get; }
+        private LavalinkNodeConnection Lavalink { get; set; }
+        private LavalinkGuildConnection LavalinkVoice { get; set; }
+
+
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
+        }
+
+        [Command("github")]
+        public async Task Github(CommandContext ctx)
+        {
+            await ctx.RespondAsync("https://github.com/thijsrijkers/Rasper");
+        }
+
+        [Command("advise")]
+        public async Task musicAdvise(CommandContext ctx)
+        {
+            await ctx.RespondAsync("https://www.youtube.com/watch?v=F3IwBMuDtu8");
+            await ctx.RespondAsync("https://www.youtube.com/watch?v=FCesTBJFBwE");
+            await ctx.RespondAsync("https://www.youtube.com/watch?v=XEC2avceTxY");
+        }
+    }
+}
---
+++ Rasper/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Lavalink;
+using DSharpPlus.Net;
+
+namespace Rasper
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextExtension commands;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "---ENTER YOUR BOT TOKEN HERE---",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefixes = new List<string> {"!"}
+            });
+
+            commands.RegisterCommands<Command>();
+            commands.RegisterCommands<LavaLink>();
+            var Lavalink = discord.UseLavalink();
+             
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
